2feca5267e9a204d40127a7564418e5850cb7f36
hyperball123 <shaikhsufiyan810@gmail.com>
2023-03-01 19:19:06 +0530

    project completed

--- src/components/article/Article.jsx
+++ src/components/article/Article.jsx
@@ -1,12 +1,21 @@
-import React from 'react';
-import './article.css';
+import React from "react";
+import "./article.css";
 
-const article = () => {
+const article = ({ imgUrl, date, title }) => {
   return (
-    <div>
-      Article
+    <div className="gpt3__blog-container_article">
+      <div className="gpt3__blog-container_article-img">
+        <img src={imgUrl} alt="blog" />
+      </div>
+      <div className="gpt3__blog-container_article-content">
+        <div>
+          <p>{date}</p>
+          <h3>{title}</h3>
+        </div>
+        <p>Read Full Article</p>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default article
+export default article;
